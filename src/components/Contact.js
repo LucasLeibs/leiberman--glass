6716d1ccc53afb4acc450745837efada4a679621
LucasLeibs <dr.lucasleibs@gmail.com>
2021-03-05 11:22:48 -0800

    added notification when email is sent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import MailOutlineRoundedIcon from "@material-ui/icons/MailOutlineRounded";
 import Button from "@material-ui/core/Button";
 import emailjs from "emailjs-com";
 import logo from '../images/logo.png'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Contact() {
+
+  const [name, setName] = useState("")
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
@@ -17,8 +21,14 @@ export default function Contact() {
         }
       );
       e.target.reset()
+      toast.dark(`Thank you ${name}! We will get back to you shortly!`)
   };
 
+  const trackName = (e) => {
+    setName(e.target.value)
+    console.log(name)
+  }
+ 
   return (
     <section id="contact">
      
@@ -28,11 +38,22 @@ export default function Contact() {
        </Button> */}
        <div className="form-container">
        <h2>INQUIRE</h2>
+       <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={true}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover={false}
+/>
       <form onSubmit={(e) => sendEmail(e)} id="form" class="topBefore">
-        <input id="name" type="text" placeholder="NAME" name="name" />
+        <input  onChange={(e) => trackName(e)} id="name" type="text" placeholder="NAME" name="name" />
         <input id="email" type="text" placeholder="E-MAIL" name="email" />
         <textarea id="message" type="text" placeholder="MESSAGE" name="message"></textarea>
-        <input id="submit" type="submit" value="GO!" />
+        <input  id="submit" type="submit" value="GO!" />
       </form>
       </div>
     </section>
